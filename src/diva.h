88adcb520bd1ba6e37f903ec3f0860b63c2242b2
ehoclover <ehoclover@gmail.com>
2025-04-16 00:39:44 -0300

    Prevent chance time frames from appearing in PV viewing mode

--- src/diva.h
+++ src/diva.h
@@ -1038,6 +1038,7 @@ inline prj::vector<prj::string>* GetRomDirectories()
 namespace game
 {
 	inline bool IsFutureToneMode() { return *reinterpret_cast<bool*>(0x1414AB9E3); }
+	inline bool IsPvMode() { return GetPvGameplayInfo()->type == 3 || GetPvGameplayInfo()->type == 6; }
 
 	// NOTE: The CustomizeSel doesn't have duplicated code for the Switch version
 	//       like other UI screens, so this is valid for both Switch and PS4 mode.
--- src/game/game.cpp
+++ src/game/game.cpp
@@ -422,10 +422,13 @@ HOOK(void, __fastcall, UpdateLife, 0x140245220, PVGameData* a1, int32_t hit_stat
 
 HOOK(void, __fastcall, ExecuteModeSelect, 0x1503B04A0, PVGamePvData* pv_data, int32_t op)
 {
-	int32_t op_difficulty = pv_data->script_buffer[pv_data->script_pos + 1];
-	int32_t difficulty = 1 << GetPvGameplayInfo()->difficulty;
-	if ((op_difficulty & difficulty) != 0)
-		SetChanceTimeMode(&pv_data->pv_game->ui, pv_data->script_buffer[pv_data->script_pos + 2]);
+	if (!game::IsPvMode())
+	{
+		int32_t op_difficulty = pv_data->script_buffer[pv_data->script_pos + 1];
+		int32_t difficulty = 1 << GetPvGameplayInfo()->difficulty;
+		if ((op_difficulty & difficulty) != 0)
+			SetChanceTimeMode(&pv_data->pv_game->ui, pv_data->script_buffer[pv_data->script_pos + 2]);
+	}
 
 	return originalExecuteModeSelect(pv_data, op);
 }
