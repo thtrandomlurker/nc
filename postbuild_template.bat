09a12b0e9795ca94b0bec73bfcac710a4b8a7de9
ehoclover <ehoclover@gmail.com>
2025-08-19 11:03:21 -0300

    Move copy directives from the project file to an external script

---
+++ postbuild_template.bat
@@ -0,0 +1,6 @@
+@echo off
+rem   This script runs after the build has finished.
+rem   # ---------------------------------------------------------------------------------#
+rem   To copy the DLL directly into the mod directory of your game (for quick debugging),
+rem   uncomment the following line (remove `rem`) and set your game's path accordingly.
+rem copy /y ".\x64\Release\NewClassics.dll" "<game path here>\mods\New Classics\NewClassics.dll"
